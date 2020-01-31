75e258fb582ca11ecdc5cf38746e5744e6f9dd9a
Joe Gerhard <jlgerhard@gmail.com>
2020-01-31 15:22:46 -0600

    Add link icons and theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,14 @@
 
 export type Theme = {
-  readonly primary: '#999999';
-  readonly accent: '#FF0000';
-  readonly light: '#EEEEEE';
+  readonly primary: '#759FF8';
+  readonly accent: '#DD6A67';
+  readonly light: '#D8D8D8';
   readonly dark: '#111111';
 }
 
 export const theme: Theme = {
-  primary: '#999999',
-  accent: '#FF0000',
-  light: '#EEEEEE',
+  primary: '#759FF8',
+  accent: '#DD6A67',
+  light: '#D8D8D8',
   dark: '#111111',
 }
