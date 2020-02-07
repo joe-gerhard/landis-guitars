75626ab6e7b9ecb8f44f7316ccb2bb18ec8aab2f
Joe Gerhard <jlgerhard@gmail.com>
2020-02-07 12:37:58 -0600

    Add dropdown animation to menu

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Styled } from './styles';
 
 const MenuBar: React.FC = () => {
+
+  const [ menuOpen, setMenuOpen ] = useState<boolean>(false);
+
   return (
-    <Styled.MenuBar>
-      Menu
-    </Styled.MenuBar>
+    <>
+      <Styled.MenuBar onClick={() => setMenuOpen(!menuOpen)}>
+        Menu
+      </Styled.MenuBar>
+      <Styled.Dropdown open={menuOpen}>
+        <span>Home</span>
+        <span>About</span>
+        <span>Blog</span>
+        <span>Events</span>
+      </Styled.Dropdown>
+    </>
   )
 }
 
--- src/components/MenuBar/styles.ts
+++ src/components/MenuBar/styles.ts
@@ -2,18 +2,41 @@ import styled, { css } from 'styled-components';
 import { Theme } from '../../styles/theme';
 
 type MenuBarProps = {
-  theme: Theme
+  theme: Theme;
+  open?: boolean;
 }
 
 const MenuBar = styled('nav')<MenuBarProps>(({ theme }) => css`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background: black;
   color: white;
-  height: 36px;
+  min-height: 36px;
+  transition: .5s;
 `);
+
+const Dropdown = styled('nav')<MenuBarProps>(({ theme, open }) => css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: ${open ? 200 : 0}px;
+  background: black;
+  color: white;
+  width: 100%;
+  transition: .5s;
+  overflow: hidden;
+
+  span {
+    opacity: ${open ? 1 : 0};
+    transition: .3s;
+  }
+`);
+
 
 export const Styled = {
   MenuBar,
+  Dropdown,
 }
