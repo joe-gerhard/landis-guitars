cffdad5c4f062bddb510b62fd66ed6916c7e86c9
Joe Gerhard <jlgerhard@gmail.com>
2020-02-08 10:31:50 -0600

    Add rules to CSS reset

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,7 +4,12 @@ export default createGlobalStyle`
   * {
     padding: 0;
     margin: 0;
+    border-radius: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+
+    &:focus {
+      outline: none;
+    }
   }
 `;
