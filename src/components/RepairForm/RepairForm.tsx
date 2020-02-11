619896ecd51a482a1dc8e211a872a59dbced7f9b
Joe Gerhard <jlgerhard@gmail.com>
2020-02-11 17:24:10 -0600

    Add email regex to form validation

--- src/components/RepairForm/RepairForm.tsx
+++ src/components/RepairForm/RepairForm.tsx
@@ -103,8 +103,10 @@ const RepairForm : React.FC<RepairFormProps> = ({ open }) => {
 
   const validateEmail = (value: string): string => {
     let error = '';
+    let emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     if(!value) error = 'Required!'
+    if(!value.match(emailRegEx)) error = 'Invalid email format. ex: John.Doe@gmail.com'
     
     return error;
   }
