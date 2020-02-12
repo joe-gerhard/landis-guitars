50759636bd24ff258fa908d12944832334507d6e
Joe Gerhard <jlgerhard@gmail.com>
2020-02-11 18:02:14 -0600

    Fix validateEmail logic

--- src/components/RepairForm/RepairForm.tsx
+++ src/components/RepairForm/RepairForm.tsx
@@ -106,7 +106,7 @@ const RepairForm : React.FC<RepairFormProps> = ({ open }) => {
     let emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     if(!value) error = 'Required!'
-    if(!value.match(emailRegEx)) error = 'Invalid email format. ex: John.Doe@gmail.com'
+    else if(!value.match(emailRegEx)) error = 'Invalid email format. ex: John.Doe@gmail.com'
     
     return error;
   }
